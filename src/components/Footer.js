41fac45a37846f918c5a5485866677e51c2dd5a2
Esra-Alhussain <esra.alhussain.s@gmail.com>
2024-04-16 20:45:38 -0700

    setup the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
- 
+
 export default function Footer() {
 
     return(
-        <div>
+        <div className='footer-section'>
             
+          <div className="social-icons">
+            <i class="fab fa-twitter" id="icon-twitter" />
+            <i class="fab fa-facebook" id="icon-facebook" />
+            <i class="fab fa-github" id="icon-github" />
+            <i class="fab fa-instagram" id="icon-instagram" />
+          </div>
         </div>
     )
 }
+
--- src/components/Info.js
+++ src/components/Info.js
@@ -3,13 +3,14 @@ import PersonImage from "../images/person-img.jpg";
 import "../index.css";
 import About from "./About";
 import Interests from "./Interests";
+import Footer from './Footer';
 
 export default function Info() {
 
     return(
         <div className='card'>
             <div className='card-info'>
-                <img className='person-img' src={PersonImage} />
+                <img className='person-img' alt="person-img" src={PersonImage} />
                 <h3 className='person-name'>Esraa Alhussain</h3>
                 <p className='person-position'>Frontend Developer</p>
                 <p className='person-email'>esra.alhussain.s@gmail.com</p>
@@ -18,6 +19,7 @@ export default function Info() {
                 <button class="linkedIn-button"> <i class="fab fa-linkedin" id="linkedin-icon"></i>LinkedIn</button>
                 <About />
                 <Interests />
+                <Footer />
             </div>
         </div>
     )
